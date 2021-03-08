bf498803309321a2ced86569a110cd67d5c8a87f
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-08 10:50:47 +0400

    chore: add utils to access database

---
+++ src/utils/db.ts
@@ -0,0 +1,45 @@
+import * as mongoose from 'mongoose';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+let cachedDb: typeof mongoose;
+
+const dbConnect = async () => {
+  // check if we have a connection to the database or if it's currently
+  // connecting or disconnecting (readyState 1, 2 and 3)
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error('MONGODB_URI is not set')
+  }
+
+  if (cachedDb) {
+    console.log('=> using cached database instance');
+    return Promise.resolve(cachedDb);
+  }
+
+  const connection = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+
+  cachedDb = connection;
+
+  return connection;
+};
+
+export const withDb = (handler: any) => async (req: NextApiRequest, res: NextApiResponse) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
+  await dbConnect();
+
+  return handler(req, res);
+};
+
+export default dbConnect;
