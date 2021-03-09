ae94dd93455204740f0dd95970bdbb2b51a555a3
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-09 07:58:32 +0400

    chore: get data directly from database instead of api

--- src/modules/Common/pages/home.tsx
+++ src/modules/Common/pages/home.tsx
@@ -1,28 +1,17 @@
 import Search, { SearchProps } from 'components/Search';
 import { useRouter } from 'next/router';
 
-import { GetHashtagsResponse } from '../interfaces';
 import LastHashtags from '../data-components/LastHashtags';
 import Layout from 'modules/Common/components/Layout';
 import axios from 'axios';
-import { mutate } from 'swr';
 import styles from 'modules/DesignSystem/pages/index.module.scss';
 
 const HomePage = () => {
   const router = useRouter();
   const onSubmit: SearchProps['onSubmit'] = async (hashtag) => {
-    mutate(
-      '/api/hashtags',
-      (data: GetHashtagsResponse) => ({
-        ...data,
-        hashtags: [...data.hashtags, { name: hashtag }],
-      }),
-      false
-    );
     try {
       await axios.post('/api/hashtags', { name: hashtag });
       router.push(`/hashtags/${hashtag}`);
-      mutate('/api/hashtags');
     } catch (e) {
       console.log(''); // eslint-disable-line
       console.log('╔════START══e══════════════════════════════════════════════════'); // eslint-disable-line
