1ff0d35a361e6f3d16ac3448d3922e5cea1ab8a1
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-15 14:38:04 +0400

    chore: put static props and static path in own functions for better readability

--- src/modules/Common/pages/hashtags/[hashtag].tsx
+++ src/modules/Common/pages/hashtags/[hashtag].tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import Layout from 'modules/Embassy/components/Layout';
-import { GetStaticPaths } from 'next';
 import Link from 'next/link';
 import useSWR from 'swr';
-import dbConnect from 'utils/db';
 import VolumetryGraph, {
   VolumetryGraphOptions,
   VolumetryGraphProps,
@@ -14,8 +12,8 @@ import LanguageGraph, {
   LanguageGraphOptions,
 } from '../../components/Charts/LanguageGraph';
 import { GetHashtagResponse, Hashtag } from '../../interfaces';
-import * as HashtagManager from '../../managers/HashtagManager';
-
+export { default as getStaticProps } from './[hashtag].staticProps';
+export { default as getStaticPaths } from './[hashtag].staticPaths';
 const shouldPoll = (status: string) => ['DONE', 'DONE_ERROR', 'DONE_FIRST_FETCH'].includes(status);
 
 export default function HashtagPage({
@@ -85,96 +83,10 @@ export default function HashtagPage({
         </div>
       )}
       {languages?.length > 0 && (
-        <div style={{ height: '300px', width: '100%' }}>
+        <div style={{ height: '400px', width: '100%' }}>
           <LanguageGraph data={languages} options={{ onClick: onPieClick }} />
         </div>
       )}
     </Layout>
   );
-}
-
-export const getStaticPaths: GetStaticPaths<{
-  hashtag: string;
-}> = async () => {
-  await dbConnect();
-  const hashtags = await HashtagManager.list();
-
-  return {
-    paths: hashtags.map(({ name }) => ({ params: { hashtag: name } })), //indicates that no page needs be created at build time
-    fallback: 'blocking', //indicates the type of fallback
-  };
-};
-
-export async function getStaticProps({ params }: { params: { hashtag: string } }) {
-  await dbConnect();
-  const hashtag = await HashtagManager.get({ name: params.hashtag });
-
-  const usernames: { [key: string]: number } = {};
-  const languages: { [key: string]: number } = {};
-
-  const volumetry = hashtag.volumetry.reduce(
-    (acc: VolumetryGraphProps['data'], volumetry) => {
-      const newAcc = [...acc];
-      newAcc[0].data.push({ x: volumetry.date, y: volumetry.nbTweets || 0 });
-      newAcc[1].data.push({ x: volumetry.date, y: volumetry.nbRetweets || 0 });
-      newAcc[2].data.push({ x: volumetry.date, y: volumetry.nbLikes || 0 });
-      newAcc[3].data.push({ x: volumetry.date, y: volumetry.nbQuotes || 0 });
-      Object.keys(volumetry.languages).forEach((language) => {
-        languages[language] = (languages[language] || 0) + volumetry.languages[language];
-      });
-      Object.keys(volumetry.usernames).forEach((username) => {
-        usernames[username] = (usernames[username] || 0) + volumetry.usernames[username];
-      });
-      return acc;
-    },
-    [
-      { id: 'nbTweets', data: [] },
-      { id: 'nbRetweets', data: [] },
-      { id: 'nbLikes', data: [] },
-      { id: 'nbQuotes', data: [] },
-    ]
-  );
-
-  return {
-    props: {
-      hashtag: hashtag ? JSON.parse(JSON.stringify(hashtag, null, 2)) : null,
-      volumetry: JSON.parse(JSON.stringify(volumetry, null, 2)),
-      usernames: JSON.parse(
-        JSON.stringify(
-          Object.keys(usernames).reduce(
-            (acc: any[], username: string) => [
-              ...acc,
-              {
-                id: username,
-                label: username,
-                value: usernames[username],
-              },
-            ],
-            []
-          ),
-          null,
-          2
-        )
-      ),
-      languages: JSON.parse(
-        JSON.stringify(
-          Object.keys(languages).reduce(
-            (acc: any[], language: string) => [
-              ...acc,
-              {
-                id: language,
-                label: language,
-                value: languages[language],
-              },
-            ],
-            []
-          ),
-          null,
-          2
-        )
-      ),
-    },
-    revalidate: 300,
-    notFound: !hashtag,
-  };
 }
