c34a73e3dfd00a6f31ddca07f9363da507829814
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-16 10:25:01 +0400

    chore(Card): add component

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,3 +2,7 @@
 /// <reference types="next/types/global" />
 
 declare module '@gouvfr/all/dist/js/all.js';
+
+type WithChildren<T = {}> = T & { children?: React.ReactNode };
+
+type WithClassname<T = {}> = T & { className?: string };
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,59 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface CardProps {
+  image?: string;
+  imageAlt?: string;
+  title: React.ReactNode;
+  detail?: React.ReactNode;
+  description?: React.ReactNode;
+  href?: string;
+  horizontal?: boolean;
+  noArrow?: boolean;
+}
+
+const Card = ({
+  image,
+  imageAlt,
+  detail,
+  title,
+  href,
+  description,
+  className,
+  horizontal,
+  noArrow,
+}: WithClassname<CardProps>) => {
+  return (
+    <div
+      className={`rf-card ${className || ''} ${horizontal ? 'rf-card--horizontal' : ''} ${
+        noArrow ? 'rf-card--no-arrow' : ''
+      }`}
+    >
+      {image && (
+        <div className="rf-card__img">
+          <img src={image} alt={imageAlt} />
+        </div>
+      )}
+      <div className="rf-card__body">
+        {detail && <p className="rf-card__detail">{detail}</p>}
+
+        <h4 className="rf-card__title">
+          {href && !href.startsWith('http') && (
+            <Link href={href}>
+              <a className="rf-card__link">{title}</a>
+            </Link>
+          )}
+          {href && href.startsWith('http') && (
+            <a href={href} target="_blank" rel="noreferrer noopener" className="rf-card__link">
+              {title}
+            </a>
+          )}
+          {!href && title}
+        </h4>
+        {description && <p className="rf-card__desc">{description}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
