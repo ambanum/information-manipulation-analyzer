aa4b7a88396dcf53552250214194d59e5b34c6da
Martin Ratinaud <martinratinaud@gmail.com>
2021-04-06 10:45:22 +0400

    feat: when hashtag is DONE, still refresh the page every hour

--- src/modules/Common/pages/hashtags/[hashtag].tsx
+++ src/modules/Common/pages/hashtags/[hashtag].tsx
@@ -40,7 +40,7 @@ const shouldNotPoll = (status: string) =>
 
 const REFRESH_INTERVALS = {
   PROCESSING_PREVIOUS: 60 * 1 * 1000,
-  DONE: 0,
+  DONE: 60 * 60 * 1 * 1000,
   DONE_ERROR: 0,
   DONE_FIRST_FETCH: 0,
   PENDING: 5 * 1000,
