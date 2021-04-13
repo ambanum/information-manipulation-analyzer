a667eb1ad935f1cbcdb1c0ab0828c8ac4e31b0c7
Martin Ratinaud <martinratinaud@gmail.com>
2021-04-13 10:35:21 +0400

    chore: change docker config to test for tflmi

--- src/modules/Auth/middleware/withAuth.ts
+++ src/modules/Auth/middleware/withAuth.ts
@@ -6,10 +6,6 @@ import { shouldAuth } from '../utils';
 export const withAuth = (handler: any) => async (req: NextApiRequest, res: NextApiResponse) => {
   if (shouldAuth) {
     const session = await getSession({ req });
-    console.log(''); // eslint-disable-line
-    console.log('╔════START══session══════════════════════════════════════════════════'); // eslint-disable-line
-    console.log(session); // eslint-disable-line
-    console.log('╚════END════session══════════════════════════════════════════════════'); // eslint-disable-line
 
     if (!session) {
       res.status(401).end('Session not found');
