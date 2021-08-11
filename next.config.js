15ebf293ead2e2b9745ca9fb7f994ec7e6cf3d24
Martin Ratinaud <martinratinaud@gmail.com>
2021-08-11 14:07:17 +0400

    chore: update major packages

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const { version } = require('./package.json');
 
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   publicRuntimeConfig: {
     version,
   },
---
+++ src/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module '@gouvfr/all/dist/js/all.js';
+
+type WithChildren<T = {}> = T & { children?: React.ReactNode };
+
+type WithClassname<T = {}> = T & { className?: string };
+
+type UnboxPromise<T extends Promise<any>> = T extends Promise<infer U> ? U : never;
