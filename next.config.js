b4a016126e6c2c29be2a0fd83a173bb32a96d3c3
Martin Ratinaud <martinratinaud@gmail.com>
2021-09-14 16:36:54 +0400

    fix: add timeout on request to make sure build does not fail

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = {
   publicRuntimeConfig: {
     version,
   },
-  experimental: { staticPageGenerationTimeout: 120 },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified by next.js will be dropped. Doesn't make much sense, but how it is
