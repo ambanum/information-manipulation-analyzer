ee82d94df6cf5cc4a12db7df5edbf05c34dd98d6
Martin Ratinaud <martinratinaud@gmail.com>
2021-09-15 08:03:40 +0400

    fix: do not consider search to not exist when timeout as data will be recalculated on the frontend

--- src/modules/Common/pages/searches/[search].staticProps.ts
+++ src/modules/Common/pages/searches/[search].staticProps.ts
@@ -8,18 +8,26 @@ const MAX_EXECUTION_TIME = 60 * 1000; // 1 minute
 export default async function getStaticProps({ params }: { params: { search: string } }) {
   await dbConnect();
   let result: any = {};
+  let notFound = false;
   try {
     result = await pTimeout(
       async () => await SearchManager.getWithData({ name: params.search }),
       MAX_EXECUTION_TIME
     );
-  } catch (e) {
-    console.error(`Could not generate static props for ${params.search}: `, e.toString());
+    notFound = !result?.search;
+  } catch (e: any) {
+    if (e.name === 'TimeoutError') {
+      console.warn(`Took too long to generate static props for ${params.search}: `);
+      notFound = false;
+    } else {
+      notFound = true;
+      console.error(`Could not generate static props for ${params.search}: `, e.toString());
+    }
   }
 
   return {
     props: JSON.parse(JSON.stringify(result)),
     revalidate: 300, // seconds
-    notFound: !result?.search,
+    notFound,
   };
 }
