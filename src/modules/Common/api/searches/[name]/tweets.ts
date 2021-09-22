9379fd53a01a2a06637fda82b6ac05f8bf43b289
Martin Ratinaud <martinratinaud@gmail.com>
2021-09-22 17:06:11 +0400

    chore: fix useSplitSwr not refreshing correctly

---
+++ src/modules/Common/api/searches/[name]/tweets.ts
@@ -0,0 +1,49 @@
+import * as SearchManager from '../../../managers/SearchManager';
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { GetSearchTweetsResponse } from 'modules/Common/interfaces';
+import HttpStatusCode from 'http-status-codes';
+import { withAuth } from 'modules/Auth';
+import { withDb } from 'utils/db';
+
+const getTweets =
+  (filter: { name: string; min?: string; max?: string }) =>
+  async (res: NextApiResponse<GetSearchTweetsResponse>) => {
+    try {
+      const search = await SearchManager.get({ name: filter.name });
+
+      if (!search) {
+        res.statusCode = HttpStatusCode.NOT_FOUND;
+        res.json({ status: 'ko', message: 'Search was not found' });
+        return;
+      }
+      const tweets = await SearchManager.getTweets({
+        searchIds: [search._id],
+        startDate: filter.min,
+        endDate: filter.max,
+      });
+
+      if (filter.min && filter.max) {
+        res.setHeader('Cache-Control', `max-age=${10 * 60}`);
+      }
+      res.statusCode = HttpStatusCode.OK;
+      res.json({ status: 'ok', message: 'Search Tweets details', ...tweets });
+      return res;
+    } catch (e) {
+      res.statusCode = HttpStatusCode.METHOD_FAILURE;
+      res.json({ status: 'ko', message: e.toString() });
+    }
+  };
+
+const search = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET' && req.query.name) {
+    return getTweets(req.query as any)(res);
+  }
+
+  res.statusCode = HttpStatusCode.FORBIDDEN;
+  res.json({ status: 'ko', message: 'Nothing there' });
+};
+
+export default withAuth(withDb(search));
