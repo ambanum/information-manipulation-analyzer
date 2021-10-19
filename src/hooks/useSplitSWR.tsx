826c1d0d4b5d583a5bf107781db2a8a68b07ed2e
Martin Ratinaud <martinratinaud@gmail.com>
2021-10-19 14:31:38 +0400

    chore: filter correctly data taken back from server to differentiate total number of items and nb of items filtered by current request

--- src/hooks/useSplitSWR.tsx
+++ src/hooks/useSplitSWR.tsx
@@ -1,20 +1,32 @@
 import { isEqual, omit } from 'lodash/fp';
 
 import React from 'react';
+import dayjs from 'dayjs';
 import { fetcher } from 'utils/api';
 import queryString from 'query-string';
 import { usePrevious } from 'react-use';
 import useSWR from 'swr';
 
-const useSplitSWR = (splitUrl: string | null, options: any) => {
-  const [data, setData] = React.useState<any>(options.initialData);
+const useSplitSWR = (splitUrl: string | null, { initialData, ...options }: any) => {
+  const [data, setData] = React.useState<any>(initialData);
   const [error, setError] = React.useState();
   const [loading, setLoading] = React.useState(false);
+
   const { data: splitData, isValidating, error: splitError } = useSWR(splitUrl, options);
   const previousSplitUrl = usePrevious(splitUrl);
   const splitLoading = !splitData || isValidating;
 
-  const { filters: splittedPeriods, search, nbTweets } = splitData || {};
+  const urlParams = splitUrl ? queryString.parse(splitUrl.split('?')[1] || '') : {};
+
+  const {
+    filters: splittedPeriods,
+    search,
+    nbTweets,
+    nbRetweets,
+    nbLikes,
+    nbQuotes,
+    nbReplies,
+  } = splitData || {};
 
   React.useEffect(() => {
     // in case a new filter has been added, we need to show change the data
@@ -25,19 +37,57 @@ const useSplitSWR = (splitUrl: string | null, options: any) => {
     const previousParams = queryString.parse(previousSplitUrl.split('?')[1] || '');
     const params = queryString.parse(splitUrl.split('?')[1] || '');
 
-    if (
-      previousSplitUrl !== splitUrl &&
-      !isEqual(omit(['min', 'max'])(params), omit(['min', 'max'])(previousParams))
-    ) {
-      setData({
-        ...options.initialData,
-        volumetry: options.initialData.volumetry.map((vol: any) => ({
-          ...vol,
-          data: vol.data.map((d: any) => ({ ...d, y: 0 })),
-        })),
-      });
+    if (previousSplitUrl !== splitUrl) {
+      if (!isEqual(omit(['min', 'max'])(params), omit(['min', 'max'])(previousParams))) {
+        // other than min and max have change
+        setData({
+          ...initialData,
+          volumetry: initialData.volumetry.map((vol: any) => ({
+            ...vol,
+            nbTweets: 0,
+            nbRetweets: 0,
+            nbLikes: 0,
+            nbQuotes: 0,
+            nbReplies: 0,
+          })),
+        });
+      } else if (previousParams.min !== params.min || previousParams.max !== params.max) {
+        // min or max have changed, refilter
+        const newAggregatedData = {
+          ...data,
+          nbTweets: 0,
+          nbLikes: 0,
+          nbRetweets: 0,
+          nbReplies: 0,
+          nbQuotes: 0,
+          nbUsernames: 0,
+          nbAssociatedHashtags: 0,
+        };
+
+        data.volumetry.forEach((vol: any) => {
+          const volumetryDayJs = dayjs(vol.hour);
+
+          if (
+            (!urlParams.min && !urlParams.max) ||
+            (urlParams.min &&
+              urlParams.max &&
+              volumetryDayJs.isAfter(dayjs(+urlParams.min)) &&
+              volumetryDayJs.isBefore(dayjs(+urlParams.max)))
+          ) {
+            // Calculate number of tweets
+            newAggregatedData.nbTweets += vol.nbTweets;
+            newAggregatedData.nbLikes += vol.nbLikes;
+            newAggregatedData.nbRetweets += vol.nbRetweets;
+            newAggregatedData.nbReplies += vol.nbReplies;
+            newAggregatedData.nbQuotes += vol.nbQuotes;
+            newAggregatedData.nbUsernames += vol.nbUsernames;
+            newAggregatedData.nbAssociatedHashtags += vol.nbAssociatedHashtags;
+          }
+        });
+        setData(newAggregatedData);
+      }
     }
-  }, [previousSplitUrl, splitUrl]);
+  }, [previousSplitUrl, splitUrl, data, urlParams.min, urlParams.max]);
 
   React.useEffect(() => {
     // When a new split request has been found, retrieve all gathered periods
@@ -47,7 +97,18 @@ const useSplitSWR = (splitUrl: string | null, options: any) => {
 
     let aggregatedData = {
       ...data,
-      nbTweets,
+      totalNbTweets: nbTweets,
+      totalNbRetweets: nbRetweets,
+      totalNbLikes: nbLikes,
+      totalNbQuotes: nbQuotes,
+      totalNbReplies: nbReplies,
+      totalNbUsernames: 0,
+      totalNbAssociatedHashtags: 0,
+      nbTweets: 0,
+      nbRetweets: 0,
+      nbLikes: 0,
+      nbQuotes: 0,
+      nbReplies: 0,
       search,
       nbUsernames: 0,
       nbAssociatedHashtags: 0,
@@ -73,11 +134,32 @@ const useSplitSWR = (splitUrl: string | null, options: any) => {
             {}
           );
 
+          newData.volumetry.forEach((vol: any) => {
+            const volumetryDayJs = dayjs(vol.hour);
+
+            if (
+              (!urlParams.min && !urlParams.max) ||
+              (urlParams.min &&
+                urlParams.max &&
+                volumetryDayJs.isAfter(dayjs(+urlParams.min)) &&
+                volumetryDayJs.isBefore(dayjs(+urlParams.max)))
+            ) {
+              // Calculate number of tweets
+              aggregatedData.nbTweets += vol.nbTweets;
+              aggregatedData.nbLikes += vol.nbLikes;
+              aggregatedData.nbRetweets += vol.nbRetweets;
+              aggregatedData.nbReplies += vol.nbReplies;
+              aggregatedData.nbQuotes += vol.nbQuotes;
+              aggregatedData.nbUsernames += vol.nbUsernames;
+              aggregatedData.nbAssociatedHashtags += vol.nbAssociatedHashtags;
+            }
+          });
+
           aggregatedData = {
             ...aggregatedData,
             nbLoaded: aggregatedData.nbLoaded + 1,
-            nbUsernames: (aggregatedData.nbUsernames || 0) + newData.nbUsernames,
-            nbAssociatedHashtags:
+            totalNbUsernames: (aggregatedData.nbUsernames || 0) + newData.nbUsernames,
+            totalNbAssociatedHashtags:
               (aggregatedData.nbAssociatedHashtags || 0) + newData.nbAssociatedHashtags,
             volumetry: [...(aggregatedData.volumetry || []), ...newData.volumetry].sort(
               (a, b) => new Date(a.hour).getTime() - new Date(b.hour).getTime()
@@ -93,7 +175,7 @@ const useSplitSWR = (splitUrl: string | null, options: any) => {
       setLoading(false);
     };
     doFetchPeriods();
-  }, [splitLoading, setLoading, setData]);
+  }, [splitLoading, setLoading, setData, urlParams.min, urlParams.max]);
 
   return {
     data,
