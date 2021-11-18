ff28f3ab2f012c552ab92f11689733aadbacc21c
Martin Ratinaud <martinratinaud@gmail.com>
2021-11-18 10:14:13 +0400

    chore(api): add url to get graph for a given search

--- src/modules/Common/managers/SearchManager.tsx
+++ src/modules/Common/managers/SearchManager.tsx
@@ -7,7 +7,8 @@ import SearchModel, { SearchTypes } from '../models/Search';
 import TweetModel from '../models/Tweet';
 import dayjs from 'dayjs';
 import sumBy from 'lodash/fp/sumBy';
-
+import pick from 'lodash/fp/pick';
+import * as scrapeApi from '../services/scrape-api';
 interface SearchFilter {
   searchIds: string[];
   startDate?: string;
@@ -672,4 +673,29 @@ export const getTweets = async (filters: SearchFilter) => {
     mostQuotedTweets: await TweetModel.aggregate(sortTweetsAggregation({ quoteCount: -1 })),
     mostCommentedTweets: await TweetModel.aggregate(sortTweetsAggregation({ replyCount: -1 })),
   };
+};
+
+export const retrieveAndUpdateGraph = async (
+  name: string
+): Promise<Partial<Search> | undefined> => {
+  const { data: searchGraphData } = await scrapeApi.getGraph(name);
+
+  return searchGraphData;
+};
+
+export const getGraph = async (filter: { name: string }): Promise<Partial<Search>> => {
+  try {
+    const { name } = filter;
+    let search: Partial<Search> = await SearchModel.findOne(filter);
+
+    if (!search.graphUrl) {
+      // scrape it
+      search = (await retrieveAndUpdateGraph(name)) || search;
+    }
+
+    return pick(graphPickFields)(search);
+  } catch (e) {
+    console.error(e);
+    throw new Error('Could not find hashtag');
+  }
 };
