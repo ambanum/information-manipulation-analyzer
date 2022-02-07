e5baf92f6b93a56d44e68b66dddea8b77728b53d
Martin Ratinaud <martinratinaud@gmail.com>
2022-02-07 17:17:11 +0400

    feat: prevent redirecting when creating the graph as it gives a weird UI

--- src/modules/NetworkGraph/data-components/GraphCreator.tsx
+++ src/modules/NetworkGraph/data-components/GraphCreator.tsx
@@ -5,7 +5,6 @@ import { Col, Container, Row, Text, Title } from '@dataesr/react-dsfr';
 import { GraphSearchResponse } from '../interfaces';
 import useSWR from 'swr';
 import api from 'utils/api';
-import { useRouter } from 'next/router';
 import Loading from 'components/Loading';
 import Alert from 'modules/Common/components/Alert/Alert';
 import { useToggle } from 'react-use';
@@ -17,12 +16,12 @@ type GraphCreatorProps = {
 const GraphCreator: React.FC<GraphCreatorProps> = ({ search, ...props }) => {
   const encodedSearch = encodeURIComponent(search);
   const [creating, toggleCreating] = useToggle(false);
-  const router = useRouter();
-  const { data, isValidating } = useSWR<GraphSearchResponse>(`/api/graph/${encodedSearch}`);
+  const { data, isValidating, mutate } = useSWR<GraphSearchResponse>(`/api/graph/${encodedSearch}`);
   const createGraph = async () => {
     toggleCreating(true);
     await api.post(`/api/graph`, { search });
-    router.push(`/network-of-interactions-graph/${encodedSearch}`);
+    // @ts-ignore
+    mutate(`/api/graph/${encodedSearch}`);
   };
 
   const loading = isValidating && !data;
