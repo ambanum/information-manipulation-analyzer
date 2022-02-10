3058f802d41b10750b6f7fc21be88ba6066a662d
Martin Ratinaud <martinratinaud@gmail.com>
2022-02-10 10:15:56 +0400

    chore: fix typescript

--- src/modules/NetworkGraph/pages/[search].tsx
+++ src/modules/NetworkGraph/pages/[search].tsx
@@ -1,20 +1,9 @@
-import { Breadcrumb, BreadcrumbItem } from '@dataesr/react-dsfr';
-import { Col, Container, Row, Text, Title } from '@dataesr/react-dsfr';
-
-import Alert from 'modules/Common/components/Alert/Alert';
 import { GetServerSideProps } from 'next';
-import GraphDetail from 'modules/NetworkGraph/components/GraphDetail';
 import Layout from 'modules/Embassy/components/Layout';
 import React from 'react';
-import { Select } from '@dataesr/react-dsfr';
-import dayjs from 'dayjs';
-import fs from 'fs';
-import path from 'path';
-import shuffle from 'lodash/fp/shuffle';
-import useUrl from 'hooks/useUrl';
 import Graph from '../data-components/Graph';
 
-const NetworkGraphDebugPage = ({ search, files, selected: selectedInUrl }: any) => {
+const NetworkGraphDebugPage = ({ search }: any) => {
   return (
     <Layout title="Twitter network of interaction graph">
       <Graph search={search} />
