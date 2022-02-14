ad2c2a620adce18e0ee7a6648141c512b9645952
Clément Biron <clement.biron@gmail.com>
2022-02-14 16:24:41 +0100

    feat: use Callout component for graph annoucement and hide this component until the volumetry data is loaded

--- src/modules/NetworkGraph/data-components/GraphCreator.tsx
+++ src/modules/NetworkGraph/data-components/GraphCreator.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Link from 'next/link';
-import { Button } from '@dataesr/react-dsfr';
+import { Callout, CalloutText, CalloutTitle } from '@dataesr/react-dsfr';
 import { Col, Container, Row, Text, Title } from '@dataesr/react-dsfr';
-import { GraphSearchResponse } from '../interfaces';
-import useSWR from 'swr';
-import api from 'utils/api';
-import Loading from 'components/Loading';
+
 import Alert from 'modules/Common/components/Alert/Alert';
+import { Button } from '@dataesr/react-dsfr';
+import { GraphSearchResponse } from '../interfaces';
+import Link from 'next/link';
+import Loading from 'components/Loading';
+import React from 'react';
+import api from 'utils/api';
+import useSWR from 'swr';
 import { useToggle } from 'react-use';
 
 type GraphCreatorProps = {
@@ -31,39 +33,32 @@ const GraphCreator: React.FC<GraphCreatorProps> = ({ search, ...props }) => {
       <Container className="">
         <Row>
           <Col>
-            <hr />
-            <Title as="h4" look="h4">
-              Network of interaction graph
-            </Title>
-          </Col>
-        </Row>
-        <Row gutters alignItems="middle">
-          <Col n="12" className="fr-col-md-8">
-            <Text size="md" className="fr-mb-0">
-              New: you now have the possibility to generate an network of interaction graph, which
-              allows you to visually explore a narrative and replay its propagation. This feature
-              allows you to explore up to 7 days back in time.
-            </Text>
-          </Col>
-          <Col n="12" className="fr-col-md-4">
-            {loading && <Loading size="sm" />}
-            {data?.status === 'ko' && <Alert type="error">{data.error}</Alert>}
-            {data?.status === 'ok' && (
-              <>
-                {data?.searchGraph && (
-                  <Link href={`/network-of-interactions-graph/${encodedSearch}`}>
-                    <a>
-                      <Button title="create">Explore the graph</Button>
-                    </a>
-                  </Link>
-                )}
-                {!data?.searchGraph && (
-                  <Button title="create" onClick={createGraph} disabled={creating}>
-                    {creating ? <Loading size="sm" /> : 'Create now'}
-                  </Button>
-                )}
-              </>
-            )}
+            <Callout>
+              <CalloutTitle>Network of interaction graph</CalloutTitle>
+              <CalloutText size="md">
+                You now have the possibility to generate an network of interaction graph, which
+                allows you to visually explore a narrative and replay its propagation. This feature
+                allows you to explore up to 7 days back in time.
+              </CalloutText>
+              {loading && <Loading size="sm" />}
+              {data?.status === 'ko' && <Alert type="error">{data.error}</Alert>}
+              {data?.status === 'ok' && (
+                <>
+                  {data?.searchGraph && (
+                    <Link href={`/network-of-interactions-graph/${encodedSearch}`}>
+                      <a>
+                        <Button title="create">Explore the graph</Button>
+                      </a>
+                    </Link>
+                  )}
+                  {!data?.searchGraph && (
+                    <Button title="create" onClick={createGraph} disabled={creating}>
+                      {creating ? <Loading size="sm" /> : 'Create now'}
+                    </Button>
+                  )}
+                </>
+              )}
+            </Callout>
           </Col>
         </Row>
       </Container>
